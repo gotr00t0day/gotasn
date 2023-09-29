3d4aa16461413ac0ae88c1d37f779678c3b5e901
gotr00t0day <48636787+gotr00t0day@users.noreply.github.com>
2023-09-28 23:53:52 -0400

    Update gotasn.py

--- gotasn.py
+++ gotasn.py
@@ -1,11 +1,12 @@
 from colorama import Fore
+import os
 import sys
 import subprocess
 import xml.etree.ElementTree as ET
 
+
+
 banner = F"""
-
-
  _______  _______ _________ _______  _______  _         _____  
 (  ____ \(  ___  )\__   __/(  ___  )(  ____ \( (    /| / ___ \ 
 | (    \/| (   ) |   ) (   | (   ) || (    \/|  \  ( |( (   ) )
@@ -17,13 +18,14 @@ banner = F"""
                                                           (_) 
 {Fore.MAGENTA}
 by: c0d3ninja
-
-
 """
 
 print(f"{Fore.WHITE}{banner}")
 print(f"{Fore.RESET}")
- 
+
+if os.geteuid() != 0:
+    print(f"{Fore.RED}This script must be run as root!")
+    sys.exit()
 
 def scan(command: str) -> str:
     cmd = command
@@ -34,23 +36,26 @@ def scan(command: str) -> str:
 
 try:
     scan(f"./asnmap -a {sys.argv[1]} -silent > iprange.txt")
+    scan(f"sudo masscan -p80,443,8080,8443 -iL iprange.txt --rate=10000 -oX ip_addr.xml")
+
+    tree = ET.parse("ip_addr.xml")
+    root = tree.getroot()
+
+    ip_addrs = []
+    for data in root.iter('address'):
+        ip = data.get('addr')
+        print(f"{Fore.WHITE}{ip}")
+        ip_addrs.append(ip)
+
+    with open("ip_addr.txt", "a") as f:
+        for ips in ip_addrs:
+            f.writelines(f"{ips}\n")
 except IndexError:
     print(f"{Fore.RED}You must enter an argument")
     sys.exit()
 
-scan(f"sudo masscan -p80,443,8080,8443 -iL iprange.txt --rate=10000 -oX ip_addr.xml")
-
-tree = ET.parse("ip_addr.xml")
-root = tree.getroot()
-
-ip_addrs = []
-for data in root.iter('address'):
-    ip = data.get('addr')
-    print(f"{Fore.WHITE}{ip}")
-    ip_addrs.append(ip)
-
-with open("ip_addr.txt", "a") as f:
-    for ips in ip_addrs:
-        f.writelines(f"{ips}\n")
+except KeyboardInterrupt:
+        print("\nProgram interrupted.")
+        exit(0)
 
 
